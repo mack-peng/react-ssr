3b2c92cac9d9fbda75fc00b44c825d46ca5cdadd
Peng He <peng.he@strikingly.com>
2024-07-12 11:33:05 +0800

    feat(project): change project port to 8080

--- src/server.tsx
+++ src/server.tsx
@@ -50,6 +50,6 @@ app.get("*", async (req, res) => {
   res.send(content);
 });
 
-app.listen(3000, () => {
-  console.log("listening on port 3000");
+app.listen(8080, () => {
+  console.log("listening on port 8080");
 });
